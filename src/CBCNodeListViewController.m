55650ee00a02514fbbb593d51bf5bd1b005f53d1
Yarden Eitan <yar@google.com>
2018-03-12 12:07:20 -0400

    update

--- src/CBCNodeListViewController.m
+++ src/CBCNodeListViewController.m
@@ -206,6 +206,9 @@ static void CBCAddNodeFromBreadCrumbs(CBCNode *tree,
     CBCNode *child = [[CBCNode alloc] initWithTitle:title];
     [node addChild:child];
     child.metadata = metadata;
+    if ([[child.metadata objectForKey:CBCIsPrimaryDemo] boolValue]) {
+      node.metadata = child.metadata;
+    }
     if ([[child.metadata objectForKey:CBCIsDebug] boolValue] == YES) {
       tree.debugLeaf = child;
     }
